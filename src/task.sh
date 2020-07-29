b1c414e19e833b750f13e8056866968082bb72fc
gnithie <g.nithie@gmail.com>
2020-07-29 23:09:59 +0100

    [added] script

---
+++ src/task.sh
@@ -0,0 +1,11 @@
+python music_classifier.py --kernel_size 4,4 --pool_size 2,2 --modelname 44_1_22_5 --dropout2 0.5 > ../output/music_G_44_1_22_5.dat
+python music_classifier.py --kernel_size 4,4 --pool_size 2,2 --modelname 44_2_22_0 --strides 2 > ../output/music_G_44_2_22_0.dat
+python music_classifier.py --kernel_size 4,4 --pool_size 2,2 --modelname 44_2_22_5 --strides 2 --dropout2 0.5 > ../output/music_G_44_2_22_5.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,4 --modelname 94_1_34_0 > ../output/music_G_94_1_34_0.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,4 --modelname 94_1_34_5 --dropout2 0.5 > ../output/music_G_94_1_34_5.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,4 --modelname 94_2_34_0 --strides 2 > ../output/music_G_94_2_34_0.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,4 --modelname 94_2_34_5 --strides 2 --dropout2 0.5 > ../output/music_G_94_2_34_5.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,2 --modelname 94_1_32_0 > ../output/music_G_94_1_32_0.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,2 --modelname 94_1_32_5 --dropout2 0.5 > ../output/music_G_94_1_32_5.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,2 --modelname 94_2_32_0 --strides 2 > ../output/music_G_94_2_32_0.dat
+python music_classifier.py --kernel_size 9,4 --pool_size 3,2 --modelname 94_2_32_5 --strides 2 --dropout2 0.5 > ../output/music_G_94_2_32_5.dat
